b23b0d102eed63aceb687d01ff3dabfe6d347f04
evgen1205 <evgen1205@tut.by>
2015-03-28 15:45:56 +0300

    some corrections according to HTML-academy standards

--- js/my_slider.js
+++ js/my_slider.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 	if (window.innerWidth >= 650){
 		$('.slider').show();
-		var n=$('.slideWrapper').children().size();
+		var n=$('.slide_wrapper').children().size();
 		for(var i=0; i < n; i++){
-			$('.buttonContainer').append('<a class="sliderButton" id="'+i+'"></a>');
+			$('.button_container').append('<a class="slider_button" id="'+i+'"></a>');
 			}
 		var sliderTimer;
 		var slideWidth=600;
 
 	$(function(){
-		$('.slideWrapper').width($('.slideWrapper').children().size()*slideWidth);
-		var initData=parseInt($('.slideWrapper').data('current'));
+		$('.slide_wrapper').width($('.slide_wrapper').children().size()*slideWidth);
+		var initData=parseInt($('.slide_wrapper').data('current'));
 		$('#'+initData).addClass('active');
 
 		sliderTimer=setInterval(nextSlide,5000);
@@ -26,37 +26,37 @@ $(document).ready(function(){
 			prevSlide();
 		});
 
-		$('.sliderButton').click(function(){
-			$('.sliderButton').removeClass('active');
+		$('.slider_button').click(function(){
+			$('.slider_button').removeClass('active');
 			var currentSlide=parseInt($(this).attr('id'));
-			$('.slideWrapper').animate({left: -currentSlide*slideWidth},0).data('current',currentSlide);
+			$('.slide_wrapper').animate({left: -currentSlide*slideWidth},0).data('current',currentSlide);
 			$(this).addClass('active');
 		})
 	});
 
 	function nextSlide(){
-		var currentSlide=parseInt($('.slideWrapper').data('current'));
+		var currentSlide=parseInt($('.slide_wrapper').data('current'));
 		currentSlide++;
-		if(currentSlide>=$('.slideWrapper').children().size())
+		if(currentSlide>=$('.slide_wrapper').children().size())
 		{	currentSlide=0;
-			$('.slideWrapper').animate({left: -currentSlide*slideWidth},0).data('current',currentSlide);
+			$('.slide_wrapper').animate({left: -currentSlide*slideWidth},0).data('current',currentSlide);
 		} else {
-			$('.slideWrapper').animate({left: -currentSlide*slideWidth},600).data('current',currentSlide);
+			$('.slide_wrapper').animate({left: -currentSlide*slideWidth},600).data('current',currentSlide);
 		}
-		$('.sliderButton').removeClass('active');
+		$('.slider_button').removeClass('active');
 		$('#'+currentSlide).addClass('active');
 	}
 
 	function prevSlide(){
-		var currentSlide=parseInt($('.slideWrapper').data('current'));
+		var currentSlide=parseInt($('.slide_wrapper').data('current'));
 		currentSlide--;
 		if(currentSlide<0)
-		{	currentSlide=parseInt($('.slideWrapper').children().size())-1;
-			$('.slideWrapper').animate({left: -currentSlide*slideWidth},0).data('current',currentSlide);
+		{	currentSlide=parseInt($('.slide_wrapper').children().size())-1;
+			$('.slide_wrapper').animate({left: -currentSlide*slideWidth},0).data('current',currentSlide);
 		} else{
-			$('.slideWrapper').animate({left: -currentSlide*slideWidth},600).data('current',currentSlide);
+			$('.slide_wrapper').animate({left: -currentSlide*slideWidth},600).data('current',currentSlide);
 		}
-		$('.sliderButton').removeClass('active');
+		$('.slider_button').removeClass('active');
 		$('#'+currentSlide).addClass('active');
 		}
 	}
